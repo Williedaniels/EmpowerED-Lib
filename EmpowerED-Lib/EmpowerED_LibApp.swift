0334afc699cdedc9bb275ff83a9629ce7fae1004
Williedaniels <w.daniels@alustudent.com>
2024-07-22 23:05:44 +0200

    Initial Commit

---
+++ EmpowerED-Lib/EmpowerED_LibApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpowerED_LibApp.swift
+//  EmpowerED-Lib
+//
+//  Created by Willie Bless Daniels on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmpowerED_LibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
